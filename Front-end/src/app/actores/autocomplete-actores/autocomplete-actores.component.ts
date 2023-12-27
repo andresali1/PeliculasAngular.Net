59540deb7fef8aac268d250e9209280360cf513c
Andrés Salinas <andresali1@hotmail.es>
2023-12-27 09:42:41 -0500

    Agregando componente de elección r ordenamiento de actores por película

--- Front-end/src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
+++ Front-end/src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-autocomplete-actores',
@@ -14,26 +16,32 @@ export class AutocompleteActoresComponent implements OnInit {
   actores = [
     {
       nombre: 'Tom Holland',
+      personaje: '',
       foto: 'https://i.pinimg.com/474x/d9/12/d1/d912d1dfcefd58db8c689121303c8d13.jpg',
     },
     {
       nombre: 'Tom Hiddleston',
+      personaje: '',
       foto: 'https://st.depositphotos.com/1814084/5066/i/950/depositphotos_50665065-stock-photo-tom-hiddleston.jpg',
     },
     {
       nombre: 'Elizabeth Olsen',
+      personaje: '',
       foto: 'https://upload.wikimedia.org/wikipedia/commons/8/86/Elizabeth_Olsen_TIFF_2011a_%28cropped%29.jpg',
     },
     {
       nombre: 'Evangeline Lily',
+      personaje: '',
       foto: 'https://media.revistavanityfair.es/photos/60e84c0b29af8ce08356da83/master/w_1600%2Cc_limit/39618.jpg',
     },
     {
       nombre: 'Cris Evans',
+      personaje: '',
       foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Chris_Evans_SDCC_2014.jpg/800px-Chris_Evans_SDCC_2014.jpg',
     },
     {
       nombre: 'Cris Hemsworth',
+      personaje: '',
       foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Chris_Hemsworth_%288492541282%29.jpg/800px-Chris_Hemsworth_%288492541282%29.jpg',
     },
   ];
@@ -41,6 +49,10 @@ export class AutocompleteActoresComponent implements OnInit {
   actoresOriginal = this.actores;
 
   actoresSeleccionados = [];
+
+  columnasAMostrar = ['imagen', 'nombre', 'personaje', 'acciones'];
+
+  @ViewChild(MatTable) table: MatTable<any>;
 
   ngOnInit(): void {
     this.control.valueChanges.subscribe((valor) => {
@@ -54,5 +66,23 @@ export class AutocompleteActoresComponent implements OnInit {
   seleccionarOpcion(event: MatAutocompleteSelectedEvent) {
     this.actoresSeleccionados.push(event.option.value);
     this.control.patchValue('');
+    if(this.table !== undefined){
+      this.table.renderRows();
+    }
+  }
+
+  finalizaArrastre(event: CdkDragDrop<any[]>){
+    const indicePrevio = this.actoresSeleccionados.findIndex(
+      actor => actor === event.item.data
+    );
+
+    moveItemInArray(this.actoresSeleccionados, indicePrevio, event.currentIndex);
+    this.table.renderRows();
+  }
+
+  eliminar(actor){
+    const indice = this.actoresSeleccionados.findIndex(a => a.nombre == actor.nombre);
+    this.actoresSeleccionados.splice(indice, 1);
+    this.table.renderRows();
   }
 }
--- Front-end/src/app/material/material.module.ts
+++ Front-end/src/app/material/material.module.ts
@@ -12,6 +12,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTableModule } from '@angular/material/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [],
@@ -27,6 +29,8 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatNativeDateModule,
     MatTabsModule,
     MatAutocompleteModule,
+    MatTableModule,
+    DragDropModule
   ],
   imports: [CommonModule],
 })
